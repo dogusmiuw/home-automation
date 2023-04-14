87a785fe3f152dac8c25356ec4d237c1d04b5e58
Dogukan Celik <dogukancelikcse@gmail.com>
2023-04-14 20:35:27 +0300

    login page done

---
+++ _header.php
@@ -0,0 +1,32 @@
+<?php
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "web-proje";
+
+    $conn = mysqli_connect($servername, $username, $password, $dbname);
+
+    if (!$conn) {
+        die("Could not connect to database: " . mysqli_connect_error());
+    }
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Home Automation System</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet"
+          integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+    <style>
+        body, html {
+            height: 100%;
+        }
+
+        body {
+            display: grid;
+            place-items: center;
+        }
+    </style>
+</head>
+<body>
---
+++ index.php
@@ -0,0 +1,25 @@
+<?php include '_header.php' ?>
+
+    <div class="container d-flex justify-content-center">
+        <div class="row align-items-center" style="width: 1000px;border: 2px solid mediumpurple; border-radius: 10px;">
+            <div class="col" style="padding: 0">
+                <img class="img-fluid" src="img/form-cover.jpg" style="object-fit: cover; border-bottom-left-radius: 10px; border-top-left-radius: 10px;" alt="Home">
+            </div>
+            <div class="col d-flex flex-column align-items-center">
+                <h1 class="text-center">Welcome</h1>
+                <form style="width: 80%">
+                    <div class="mb-3">
+                        <label for="email" class="form-label">Email address</label>
+                        <input type="email" class="form-control" id="email" name="email">
+                    </div>
+                    <div class="mb-3">
+                        <label for="password" class="form-label">Password</label>
+                        <input type="password" class="form-control" id="password" name="password">
+                    </div>
+                    <button type="submit" class="btn btn-primary w-100">Login</button>
+                </form>
+            </div>
+        </div>
+    </div>
+
+<?php include '_footer.php' ?>
