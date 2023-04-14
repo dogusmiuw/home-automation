4e51db75daaf83024a73c457fbb3550b0f27355c
Dogukan Celik <dogukancelikcse@gmail.com>
2023-04-15 02:36:44 +0300

    asdasd

--- home.php
+++ home.php
@@ -1,7 +1,95 @@
 <?php include '_header.php' ?>
 
     <div class="container">
-
+        <div class="row">
+            <div class="col-3">
+                <div class="card"> <!-- nav -->
+                    <div class="card-body">
+                        <div class="list-group">
+                            <button type="button" class="list-group-item list-group-item-action active">Rooms</button>
+                            <button type="button" class="list-group-item list-group-item-action">Access</button>
+                            <button type="button" class="list-group-item list-group-item-action">Contact</button>
+                            <button type="button" class="list-group-item list-group-item-action">About</button>
+                            <button type="button" class="list-group-item list-group-item-action">Log out</button>
+                        </div>
+                    </div>
+                </div> <!-- nav end-->
+            </div>
+            <div class="col-9">
+                <div class="card"> <!-- main -->
+                    <div class="card-body">
+                        <h1>Rooms</h1>
+                        <div class="row mb-3">
+                            <div class="col-3">
+                                <div class="card">
+                                    <div class="card-body">
+                                        <h4>Living Room</h4>
+                                        <p><i class="fs-4 text-danger fa-solid fa-temperature-quarter"></i><span class="fs-5"> 24.5°C</span></p>
+                                        <p><i class="fs-4 text-warning fa-solid fa-lightbulb"></i><span class="fs-5"> On</span></p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-3">
+                                <div class="card">
+                                    <div class="card-body">
+                                        <h4>Bedroom</h4>
+                                        <p><i class="fs-4 text-danger fa-solid fa-temperature-quarter"></i><span class="fs-5"> 26.5°C</span></p>
+                                        <p><i class="fs-4 text-warning fa-regular fa-lightbulb"></i><span class="fs-5"> Off</span></p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-3">
+                                <div class="card">
+                                    <div class="card-body">
+                                        <h4>Bathroom</h4>
+                                        <p><i class="fs-4 text-danger fa-solid fa-temperature-quarter"></i><span class="fs-5"> 22°C</span></p>
+                                        <p><i class="fs-4 text-warning fa-regular fa-lightbulb"></i><span class="fs-5"> Off</span></p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-3">
+                                <div class="card">
+                                    <div class="card-body">
+                                        <h4>Studio</h4>
+                                        <p><i class="fs-4 text-danger fa-solid fa-temperature-quarter"></i><span class="fs-5"> 24°C</span></p>
+                                        <p><i class="fs-4 text-warning fa-regular fa-lightbulb"></i><span class="fs-5"> Off</span></p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row mb-3">
+                            <div class="col-3">
+                                <div class="card">
+                                    <div class="card-body">
+                                        <h4>Playroom</h4>
+                                        <p><i class="fs-4 text-danger fa-solid fa-temperature-quarter"></i><span class="fs-5"> 24.5°C</span></p>
+                                        <p><i class="fs-4 text-warning fa-solid fa-lightbulb"></i><span class="fs-5"> On</span></p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-3">
+                                <div class="card">
+                                    <div class="card-body">
+                                        <h4>Kitchen</h4>
+                                        <p><i class="fs-4 text-danger fa-solid fa-temperature-quarter"></i><span class="fs-5"> 28°C</span></p>
+                                        <p><i class="fs-4 text-warning fa-solid fa-lightbulb"></i><span class="fs-5"> On</span></p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-3">
+                                <div class="card">
+                                    <div class="card-body">
+                                        <h4>Balcony</h4>
+                                        <p><i class="fs-4 text-danger fa-solid fa-temperature-quarter"></i><span class="fs-5"> 18°C</span></p>
+                                        <p><i class="fs-4 text-warning fa-regular fa-lightbulb"></i><span class="fs-5"> Off</span></p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div> <!-- main end -->
+            </div>
+        </div>
     </div>
 
 <?php include '_footer.php' ?>
