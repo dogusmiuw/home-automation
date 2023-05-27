8fcce54e9bcfffc941090bfe86af9fe267fc8a84
Dogan Kirmizigul <dogankirmizigull@gmail.com>
2023-05-28 01:38:56 +0300

    js files added.

---
+++ consumer/js/graph.js
@@ -0,0 +1,9 @@
+Morris.Donut({
+    element: "pie-chart",
+    data: [
+      { label: "Air Conditioner", value: 60 },
+      { label: "Televison", value: 25 },
+      { label: "Light", value: 15 },
+    ],
+  });
+  
