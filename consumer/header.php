68040cedaf2d885f5898db4ff0202c052350f987
Dogan Kirmizigul <dogankirmizigull@gmail.com>
2023-05-30 19:41:52 +0300

    js css and php files added to mark navbar on active page.

--- consumer/header.php
+++ consumer/header.php
@@ -1,9 +1,24 @@
 <?php include("connect.php")?>
 <header>
+<script>
+  // Sayfa yüklendiğinde çalışacak JavaScript kodu
+  window.onload = function() {
+    var currentUrl = window.location.href;
+    var navLinks = document.querySelectorAll('nav ul li a');
+
+    for (var i = 0; i < navLinks.length; i++) {
+      if (navLinks[i].href === currentUrl) {
+        navLinks[i].classList.add('current');
+        break;
+      }
+    }
+  };
+</script>
+
       <div id="menu" class="fas fa-bars"></div>
       <nav class="navbar">
         <ul>
-          <li><a href="home.php">home</a></li>
+          <li><a href="home.php" class = activee>home</a></li>
           <li><a href="devices.php">devices</a></li>
           <li><a href="room.php">rooms</a></li>
           <li><a href="contact.php">contact</a></li>
