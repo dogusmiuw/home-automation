744aef24b0f8f95ba860757a3e6dc0592203e2cb
Dogan Kirmizigul <dogankirmizigull@gmail.com>
2023-05-31 12:51:12 +0300

    charts page have added.

---
+++ consumer/charts.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Plan</title>
+    <style>
+      html {
+        height: 130vh;
+      }
+      * {
+        scroll-behavior: none;
+      }
+    </style>
+    <link
+      rel="stylesheet"
+      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"
+    />
+    <link rel="stylesheet" href="css/styles.css" />
+  </head>
+  <body>
+   
+  <?php include("header.php") ?>
